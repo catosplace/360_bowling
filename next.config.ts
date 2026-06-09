9e78f26ca61494a8b923863f9d58fb7988358d68
Peter Sellars <psellars@gmail.com>
2026-06-09 23:54:40 +1200

    fix: build for container set as output standalone

--- next.config.ts
+++ next.config.ts
@@ -29,6 +29,7 @@ const nextConfig: NextConfig = {
   turbopack: {
     root: path.resolve(dirname),
   },
+  output: 'standalone',
 }
 
 export default withPayload(nextConfig, { devBundleServerPackages: false })
